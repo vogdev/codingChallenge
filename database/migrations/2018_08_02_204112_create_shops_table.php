bdf78c1299bd0f3d5f9deb19029d36d731b1eb84
vogdev <vogdev@gmail.com>
2018-08-02 23:11:57 +0100

    As a User, I can sign up using my email & password || As a User, I can sign in using my email & password

---
+++ database/migrations/2018_08_02_204112_create_shops_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('picture')->default('http://placehold.it/150x150');
+            $table->string('name');
+            $table->string('email');
+            $table->string('city');
+            $table->decimal('lat', 10, 7);
+            $table->decimal('lng', 10, 7);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ShopController@index');
+Route::get('/shops', 'ShopController@index');
+
+Auth::routes();
+
